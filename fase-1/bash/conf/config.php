deb8526ef66f7c18424095b5ad354fe51bc28664
drain113 <gmonmar2408@g.educaand.es>
2022-12-15 18:36:15 +0000

    Finalizado Ansible

--- fase-1/bash/conf/config.php
+++ fase-1/bash/conf/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'MYSQL_IP_PUB_SERVER');
-define('DB_NAME', 'lamp_db');
-define('DB_USER', 'lamp_user');
-define('DB_PASSWORD', 'lamp_pass');
+define('DB_HOST', 'DB_HOST');
+define('DB_NAME', 'DB_NAME');
+define('DB_USER', 'DB_USER');
+define('DB_PASSWORD', 'DB_PASSWORD');
 
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
