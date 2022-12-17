5c8a47d6dc82607b325bcbbd3b8c655a34fb497a
drain113 <gmonmar2408@g.educaand.es>
2022-12-17 03:31:18 +0000

    Fix

--- fase-2/bash/deploy_frontend.sh
+++ fase-2/bash/deploy_frontend.sh
@@ -48,7 +48,7 @@ cp /var/www/html/wordpress/index.php /var/www/html/index.php
 
 # Cambiamos header.php del index
 sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.php
-
+sed -i "/WP_HOME/a $_SERVER['HTTPS'] = 'on';" /var/www/html/wordpress/wp-config.php
 chown www-data:www-data /var/www/html -R
 #----------------------------------------------------------------------#
 
